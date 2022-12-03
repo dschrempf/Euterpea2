ba910d86be58b6cc963e174597e1ddf93ad6e9ca
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-03 17:49:00 +0100

    improve flake (overlay)

--- flake.nix
+++ flake.nix
@@ -10,59 +10,60 @@
     , flake-utils
     , nixpkgs
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        haskellPackageNames = [
-          "Euterpea"
-        ];
-        ghcVersion = "ghc90";
-        haskellMkPackage = hps: nm: hps.callCabal2nix nm (./. + "/${nm}") { };
-        haskellOverlay = (
-          selfn: supern: {
-            haskellPackages = supern.haskell.packages.${ghcVersion}.override {
-              overrides = selfh: superh:
-                {
-                  Euterpea = selfh.callCabal2nix "Euterpea" ./. rec { };
-                };
-            };
-          }
-        );
-        overlays = [ haskellOverlay ];
-        pkgs = import nixpkgs {
-          inherit system;
-          inherit overlays;
+    let
+      theseHpkgNames = [
+        "Euterpea"
+      ];
+      thisGhcVersion = "ghc924";
+      # # Only required for projects with multiple packages.
+      # hMkPackage = h: n: h.callCabal2nix n (./. + "/${n}") { };
+      hOverlay = selfn: supern: {
+        haskell = supern.haskell // {
+          packageOverrides = selfh: superh:
+            supern.haskell.packageOverrides selfh superh //
+              {
+                Euterpea = selfh.callCabal2nix "Euterpea" ./. { };
+              };
         };
-        hpkgs = pkgs.haskellPackages;
-        EuterpeaPkgs = nixpkgs.lib.genAttrs haskellPackageNames (n: hpkgs.${n});
-        EuterpeaPkgsDev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) EuterpeaPkgs;
-      in
-      {
-        packages = EuterpeaPkgs // { default = EuterpeaPkgs.Euterpea; };
+      };
+      perSystem = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ hOverlay ];
+          };
+          hpkgs = pkgs.haskell.packages.${thisGhcVersion};
+          hlib = pkgs.haskell.lib;
+          theseHpkgs = nixpkgs.lib.genAttrs theseHpkgNames (n: hpkgs.${n});
+          theseHpkgsDev = builtins.mapAttrs (_: x: hlib.doBenchmark x) theseHpkgs;
+        in
+        {
+          packages = theseHpkgs // { default = theseHpkgs.Euterpea; };
 
-        devShells.default = hpkgs.shellFor {
-          # shellHook =
-          #   let
-          #     scripts = ./scripts;
-          #   in
-          #   ''
-          #     export PATH="${scripts}:$PATH"
-          #   '';
-          packages = _: (builtins.attrValues EuterpeaPkgsDev);
-          nativeBuildInputs = with pkgs; [
-            # See https://github.com/NixOS/nixpkgs/issues/59209.
-            bashInteractive
+          devShells.default = hpkgs.shellFor {
+            # shellHook =
+            #   let
+            #     scripts = ./scripts;
+            #   in
+            #   ''
+            #     export PATH="${scripts}:$PATH"
+            #   '';
+            packages = _: (builtins.attrValues theseHpkgsDev);
+            nativeBuildInputs = with pkgs; [
+              # See https://github.com/NixOS/nixpkgs/issues/59209.
+              bashInteractive
 
-            # Haskell toolchain.
-            hpkgs.cabal-fmt
-            hpkgs.cabal-install
-            hpkgs.haskell-language-server
-          ];
-          buildInputs = with pkgs; [
-          ];
-          doBenchmark = true;
-          # withHoogle = true;
+              # Haskell toolchain.
+              hpkgs.cabal-fmt
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+            ];
+            buildInputs = with pkgs; [
+            ];
+            doBenchmark = true;
+            # withHoogle = true;
+          };
         };
-      }
-    );
+    in
+    { overlays.default = hOverlay; } // flake-utils.lib.eachDefaultSystem perSystem;
 }
